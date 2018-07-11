ac91622b50aceca1cca238764a56cccf1f0a4b3d
syt123450 <421537891@qq.com>
2018-07-11 21:50:01 +0800

    update build file

--- game.js
+++ game.js
@@ -3,9 +3,9 @@ import './js/symbol'
 var data = require('./data/sampleData.js');
 
 // use beautiful file in development environment
-var Gio = require('./js/gioWX.js');
+var Gio = require('./js/gio.wx.js');
 // use min file in production environment
-// var Gio = require('./js/gioWX.min.js');
+// var Gio = require('./js/gio.wx.min.js');
 var controller = new Gio.Controller(canvas);
 controller.addData(data);
 controller.init();
