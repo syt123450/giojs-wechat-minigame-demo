c0848f8610695ae7c2dc9a3f1fd546db7fdd779c
syt123450 <421537891@qq.com>
2018-07-11 19:44:11 +0800

    adjust code level

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-import './js/libs/weapp-adapter'
-import './js/libs/symbol'
+import './js/weapp-adapter'
+import './js/symbol'
 var data = require('./data/sampleData.js');
 
 // use beautiful file in development environment
-var Gio = require('./js/libs/gioWX.js');
+var Gio = require('./js/gioWX.js');
 // use min file in production environment
-// var Gio = require('./js/libs/gioWX.min.js');
+// var Gio = require('./js/gioWX.min.js');
 var controller = new Gio.Controller(canvas);
 controller.addData(data);
 controller.init();
